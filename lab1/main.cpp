3d45847423e17109e688db25e72c630714b4f77d
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2024-09-12 00:16:22 +0300

    update

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -2,21 +2,21 @@
 #include <string>
 #include <stdexcept>
 #include <cwchar>
-PROCESS_INFORMATION create_process(std::string process_name)
+PROCESS_INFORMATION create_process(const std::string& process_name)
 {
   STARTUPINFO si;
   PROCESS_INFORMATION pi;
   ZeroMemory(&si, sizeof(si));
   si.cb = sizeof(si);
   ZeroMemory(&pi, sizeof(pi));
-  if (!CreateProcess(NULL,// No module name (use command line)
+  if (!CreateProcess(nullptr,// No module name (use command line)
                      const_cast<char *>(process_name.c_str()),
-                     NULL,           // Process handle not inheritable
-                     NULL,           // Thread handle not inheritable
-                     FALSE,          // Set handle inheritance to FALSE
+                     nullptr,           // Process handle not inheritable
+                     nullptr,           // Thread handle not inheritable
+                     false,          // Set handle inheritance to FALSE
                      0,              // No creation flags
-                     NULL,           // Use parent's environment block
-                     NULL,           // Use parent's starting directory
+                     nullptr,           // Use parent's environment block
+                     nullptr,           // Use parent's starting directory
                      &si,            // Pointer to STARTUPINFO structure
                      &pi)           // Pointer to PROCESS_INFORMATION structure
     )
